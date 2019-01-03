8cd730f9a0e803b48b07aa9c54200cb01c10abc2
rodrigotorrico <rodrigo.torricodelcastillo@hotmail.com>
2019-01-03 12:02:27 -0400

    implemented standard routing of the page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: NotFoundPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
